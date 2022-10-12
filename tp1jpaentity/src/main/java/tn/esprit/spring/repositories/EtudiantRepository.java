76901783dee676546e929055d48c88b34e8f96a0
Zeineb Hamdi <61847277+zeineeb@users.noreply.github.com>
2022-10-12 11:55:08 +0100

    CRUD
    Service +
    Repository+
    Controllers

---
+++ tp1jpaentity/src/main/java/tn/esprit/spring/repositories/EtudiantRepository.java
@@ -0,0 +1,11 @@
+package tn.esprit.spring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tn.esprit.spring.entity.Etudiant;
+
+@Repository
+public interface EtudiantRepository extends JpaRepository<Etudiant, Long> {
+
+
+}
